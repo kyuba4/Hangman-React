09a967ab94c2d4a453429be6fa413d9b80982498
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2021-12-10 18:35:53 +0100

    Fetching word from API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { useState, useEffect } from "react";
 import Keyboard from "./components/Keyboard";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 
 function App() {
-  const handleClick = (key) => {
-    console.log(key);
+  const maxChances = 6;
+
+  const fetchWord = async () => {
+    const response = await fetch("https://random-words-api.vercel.app/word");
+    const data = await response.json();
+
+    setWord(data[0].word);
   };
+
+  const handleClick = (key) => {
+    setImage(image + 1);
+    setChancesLeft(chancesLeft - 1);
+  };
+
+  useEffect(() => {
+    fetchWord();
+  }, []);
+
+  const [image, setImage] = useState(0);
+  const [chancesLeft, setChancesLeft] = useState(maxChances);
+  const [word, setWord] = useState("");
 
   return (
     <div>
@@ -12,6 +31,11 @@ function App() {
       <div className="switch-wrap">
         <ThemeSwitcher />
       </div>
+      <div className="chances-left">Chances Left: {chancesLeft}</div>
+      <div className="hangman-img">
+        <img src={`assets/${image}.jpg`} alt="Hangman" />
+      </div>
+      <div className="word">{word}</div>
       <Keyboard handleClick={handleClick} />
     </div>
   );
